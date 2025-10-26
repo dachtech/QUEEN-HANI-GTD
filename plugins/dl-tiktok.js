829d5dc52e5fc6da56d32877e3c3486f53c9b444
dachtech <maamdach@gmail.com>
2025-10-26 18:58:16 +0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -34,7 +34,7 @@ const tiktok = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363290715861418@newsletter",
+          newsletterJid: "120363401831624774@newsletter",
           newsletterName: "QUEEN HANI GTD",
           serverMessageId: 143,
         },
